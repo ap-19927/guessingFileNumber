1b1bf4dbec941fed3224a284669643fcb789ad20
allmannpatrick <allmannpatrick@gmail.com>
2021-06-04 16:56:35 -0700

    mod gg.sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Project title: guessingFileNumber
  
 The date and time at which make was run:
-2021-06-04 14:59:01
+2021-06-04 16:55:39
  
 The number of lines of code contained in guessinggame.sh:
-4
+30
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,4 +1,30 @@
-expr 5+5
-f
-f
-f
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+num_lin=$(ls -A | wc -l) #stores number of lines in current directory, ls -A lists each file on a separate line, piped to wc -l, which counts the number of lines.
+
+echo "Please enter the number of files in the current directory: "
+read response
+echo "You entered $response."
+
+while [[ $response -ne $num_lin ]]
+do
+    if [[ $response -gt $num_lin ]]
+    then
+        echo "Your answer is greater than the number of files in the current directory."
+        echo ""
+        echo "Please enter the number of files in the current directory: "
+        read response
+        echo "You entered $response."
+    fi
+    if [[ $response -lt $num_lin ]]
+    then
+        echo "Your answer is less than the number of files in the current directory."
+        echo ""
+        echo "Please enter the number of files in the current directory: "
+        read response
+        echo "You entered $response."
+    fi
+done
+echo ""
+echo "Congratulations. The number of files in the current directory is $response."
